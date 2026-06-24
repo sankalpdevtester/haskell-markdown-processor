ddac4d69988e57794b08f90d5a73257889d98a22
sankalpdevtester <ohofufic0@gmail.com>
2026-06-24 12:55:05 +0530

    feat: add markdown document validation service

--- src/Services/DocumentValidator.hs
+++ src/Services/DocumentValidator.hs
@@ -2,49 +2,47 @@
 module Services.DocumentValidator where
 
 import Control.Monad (liftM2)
-import Data.List (find)
-import Data.Maybe (isJust)
-import Models.MarkdownDocument (MarkdownDocument(..))
-import Models.HtmlDocument (HtmlDocument(..))
-import Utils.ErrorHandling (ValidationError(..), throwValidationError)
-import Text.Parsec (ParseError, parse)
+import Data.Maybe (isJust, fromJust)
+import Text.Parsec (ParseError)
+import Text.Parsec.Error (errorPos)
+
+import qualified Models.MarkdownDocument as MD
+import qualified Utils.ErrorHandling as EH
+import qualified Services.MarkdownConverter as MC
 
 -- | Validate a Markdown document
-validateMarkdownDocument :: MarkdownDocument -> Either ValidationError MarkdownDocument
-validateMarkdownDocument doc
-  | null (mdTitle doc) = Left $ ValidationError "Title cannot be empty"
-  | null (mdContent doc) = Left $ ValidationError "Content cannot be empty"
-  | otherwise = Right doc
+validateDocument :: MD.MarkdownDocument -> Either EH.AppError MD.MarkdownDocument
+validateDocument doc = do
+  -- Check if the document has a title
+  when (null $ MD.title doc) $ Left EH.MissingTitleError
 
--- | Validate an HTML document
-validateHtmlDocument :: HtmlDocument -> Either ValidationError HtmlDocument
-validateHtmlDocument doc
-  | null (htmlTitle doc) = Left $ ValidationError "Title cannot be empty"
-  | null (htmlContent doc) = Left $ ValidationError "Content cannot be empty"
-  | otherwise = Right doc
+  -- Check if the document has content
+  when (null $ MD.content doc) $ Left EH.EmptyDocumentError
 
--- | Validate a Markdown document and convert it to HTML
-validateAndConvertMarkdown :: MarkdownDocument -> Either ValidationError HtmlDocument
-validateAndConvertMarkdown doc = do
-  validatedDoc <- validateMarkdownDocument doc
-  let htmlDoc = convertMarkdownToHtml validatedDoc
-  validateHtmlDocument htmlDoc
-
--- | Convert a Markdown document to HTML
-convertMarkdownToHtml :: MarkdownDocument -> HtmlDocument
-convertMarkdownToHtml doc = HtmlDocument
-  { htmlTitle = mdTitle doc
-  , htmlContent = mdContent doc
-  }
+  -- Try to parse the Markdown content
+  case MC.parseMarkdown (MD.content doc) of
+    Left err -> Left $ EH.ParseError $ show err
+    Right _ -> Right doc
 
 -- | Validate a list of Markdown documents
-validateMarkdownDocuments :: [MarkdownDocument] -> Either ValidationError [MarkdownDocument]
-validateMarkdownDocuments docs = mapM validateMarkdownDocument docs
+validateDocuments :: [MD.MarkdownDocument] -> Either EH.AppError [MD.MarkdownDocument]
+validateDocuments docs = mapM validateDocument docs
 
--- | Example usage
-exampleUsage :: IO ()
-exampleUsage = do
-  let doc = MarkdownDocument "Example Title" "Example Content"
-  case validateMarkdownDocument doc of
-    Left err -> print err
-    Right validatedDoc -> print validatedDoc
+-- | Check if a document has a valid title
+hasValidTitle :: MD.MarkdownDocument -> Bool
+hasValidTitle doc = not $ null $ MD.title doc
+
+-- | Check if a document has valid content
+hasValidContent :: MD.MarkdownDocument -> Bool
+hasValidContent doc = not $ null $ MD.content doc
+
+-- | Get the validation errors for a document
+getValidationErrors :: MD.MarkdownDocument -> [EH.AppError]
+getValidationErrors doc
+  | null (MD.title doc) = [EH.MissingTitleError]
+  | null (MD.content doc) = [EH.EmptyDocumentError]
+  | otherwise = []
+
+-- | Example usage:
+-- validateDocument (MD.MarkdownDocument "Example" "This is an example document.")
+-- validateDocuments [MD.MarkdownDocument "Example 1" "This is an example document.", MD.MarkdownDocument "" "This is another example document."]
